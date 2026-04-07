22cf0d9348b3f75559b8ad7a63c25921da260f18
farzad-cell <farzaddarwazi869@gmail.com>
2026-04-06 19:25:35 -0700

    name fix for passwordVerifier.cpp

---
+++ Movie Analyzer v2/movieAnalyzerV2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Welcome to Movie Analyzer V2!" << std::endl;
+    // Additional code for movie analysis will go here
+    int movieCount;
+    std::cin >> movieCount; // Wait for user input before closing the console
+    std::cout << "You entered: " << movieCount << std::endl;
+    return 0;
+}
